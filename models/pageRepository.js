fccfb88fdbb501d61189d5570a51021b96313749
nardinmarcus <35812495+nardinmarcus@users.noreply.github.com>
2026-07-14 20:45:08 +0800

    ci: isolate tests from production data

--- models/pageRepository.js
+++ models/pageRepository.js
@@ -1,4 +1,9 @@
 function createPageRepository() {
+  if (process.env.NODE_ENV === 'test') {
+    const { MemoryPageRepository } = require('./memory-pages');
+    return new MemoryPageRepository();
+  }
+
   if (process.env.DATABASE_URL || process.env.POSTGRES_URL) {
     const { PostgresPageRepository } = require('./postgres-pages');
     return new PostgresPageRepository(process.env.DATABASE_URL || process.env.POSTGRES_URL);
